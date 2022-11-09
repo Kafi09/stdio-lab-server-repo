da4da43aa3ddac8ff599dab71a1d63e971095732
Kafi09 <kaficsmy9@gmail.com>
2022-11-09 23:16:11 +0600

    added review

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middle wares
+
 app.use(cors());
 app.use(express.json());
 
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('stdioDb').collection('services');
-        
+        const reviewCollection = client.db('stdioDb').collection('reviews');
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -27,8 +27,15 @@ async function run() {
             res.send(services);
         });
 
-        
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
 
+
+        
 
     }
     finally {

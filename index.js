c091118b224a916adc2babc55b4eb6cefe144cf1
Kafi09 <kaficsmy9@gmail.com>
2022-11-10 17:02:29 +0600

    added review details

--- index.js
+++ index.js
@@ -36,7 +36,18 @@ async function run() {
 
 
         
-        
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
 
 
         app.post('/reviews', async (req, res) => {
@@ -46,6 +57,9 @@ async function run() {
         });
 
         
+
+        
+
 
     }
     finally {
